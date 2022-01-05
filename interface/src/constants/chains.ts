69a6df6ff490b0cda48cd28da35f6e1b403fd214
moody <ian@IB-Macbook.local>
2022-01-04 19:52:15 -0800

    constants

--- interface/src/constants/chains.ts
+++ interface/src/constants/chains.ts
@@ -1,7 +1,15 @@
 export enum SupportedChainId {
-    MAINNET = 1,
-    RINKEBY = 4,
-    FANTOM = 250,
-    BSC = 56,
-    POLYGON = 137,
-  }
+  MAINNET = 1,
+  RINKEBY = 4,
+  FANTOM = 250,
+  BSC = 56,
+  POLYGON = 137,
+}
+
+export const ALL_SUPPORTED_CHAIN_IDS: SupportedChainId[] = [
+  SupportedChainId.MAINNET,
+  SupportedChainId.RINKEBY,
+  SupportedChainId.FANTOM,
+  SupportedChainId.BSC,
+  SupportedChainId.POLYGON,
+];
